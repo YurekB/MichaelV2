1e919d8d9efef6bd6fa4f20a55e306284b5e8de2
YurekB <boguckiyurek@gmail.com>
2023-01-18 13:59:30 +0000

    foter email setup

--- michael/src/componenets/Footer.jsx
+++ michael/src/componenets/Footer.jsx
@@ -4,7 +4,10 @@ const Footer = () => {
       <h3>Michael Cupial Architect</h3>
       <div className="footer-details">
         <p>21 Sunnyfield Road Prestwich, M25 2RD</p>
-        <p>07742 524783 michael@cupial.co.uk</p>
+        <p>
+          07742 524783{" "}
+          <a href="mailto:michael@cupial.co.uk">michael@cupial.co.uk</a>
+        </p>
       </div>
     </footer>
   );
